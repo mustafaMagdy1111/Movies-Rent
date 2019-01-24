3c574bf3468a112c64e79dc71a95ccba5367f616
mustafa magdy <mustafa.magdy1111@gmail.com>
2019-01-24 15:06:32 +0200

    Add project files.

---
+++ Movies Rent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movies_Rent.Startup))]
+namespace Movies_Rent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
